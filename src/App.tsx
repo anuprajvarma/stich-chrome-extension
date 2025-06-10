9c55e9f71acb5430b991f6e6c7cc90e430ed0f02
anuprajvarma <anupraj1854@gmail.com>
2025-06-11 05:01:57 +0530

    complete prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import { RiPlayListAddLine } from "react-icons/ri";
+import { MdOutlineSend } from "react-icons/md";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onclose = () => {
+    window.close();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      console.log("Key pressed:", e.key);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="top-div">
+        <div className="header">
+          <div className="stich">
+            <div>
+              <img src="icon.png" width={30} height={30} />
+            </div>
+            <h1>Stich</h1>
+          </div>
+          <button className="close-btn" onClick={onclose}>
+            X
+          </button>
+        </div>
+        <div>
+          <button className="addContext-btn">
+            <RiPlayListAddLine />
+            <p>Add Context</p>
+          </button>
+        </div>
+        <div className="text-area-div">
+          <textarea
+            className="text-area"
+            id="myTextarea"
+            placeholder="Type something..."
+            onKeyDown={(e) => handleKeyDown(e)}
+          ></textarea>
+          <div className="search-div">
+            <button>Change tone +</button>
+            <button>Length +</button>
+            <button>
+              <MdOutlineSend />
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
